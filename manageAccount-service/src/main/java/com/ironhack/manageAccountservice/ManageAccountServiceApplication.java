c4304a107167900d4552daea5e66de2a109c66f8
Iván Trujillo <ivtrujillo12@gmail.com>
2021-02-18 09:07:42 +0000

    Added first projects

---
+++ manageAccount-service/src/main/java/com/ironhack/manageAccountservice/ManageAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.manageAccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageAccountServiceApplication.class, args);
+	}
+
+}
---
+++ manageAccount-service/src/test/java/com/ironhack/manageAccountservice/ManageAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.manageAccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
